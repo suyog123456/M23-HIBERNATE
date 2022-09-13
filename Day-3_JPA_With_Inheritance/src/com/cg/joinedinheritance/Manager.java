0831fbd74c9fd81951dfcf905bd8897ccbd6ec63
HP <HP@LAPTOP-LRS23TOA>
2022-09-13 09:00:47 +0530

    First Commit

---
+++ Day-3_JPA_With_Inheritance/src/com/cg/joinedinheritance/Manager.java
@@ -0,0 +1,24 @@
+package com.cg.joinedinheritance;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Manager2")
+public class Manager extends Employee
+{
+	private static final long serialVersionUID = 1L;
+	private String deptname;
+	
+	//getters and setters method
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	
+	
+
+}
---
+++ Day-3_JPA_With_Inheritance/src/com/cg/singleinheritance/Client.java
@@ -0,0 +1,42 @@
+package com.cg.singleinheritance;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em=factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create an employee1
+		Employee e=new Employee();
+		e.setName("Monika");
+		e.setSalary(35000);
+		em.persist(e);
+		
+		//create an employee1
+		Employee e1=new Employee();
+		e1.setName("Priyanka");
+		e1.setSalary(25000);
+		em.persist(e1);
+		
+		//create a manager
+		Manager m=new Manager();
+		m.setName("Sumit");
+		m.setSalary(55000);
+		m.setDeptname("Information Technology");
+		em.persist(m);
+		
+		em.getTransaction().commit();
+		System.out.println("The data is added");
+		em.close();
+		factory.close();
+		
+		
+	}
+	
+
+}
